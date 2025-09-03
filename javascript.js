744d4d87c9e019e92965f8b8c7f7da479a13f5e2
James Collins <jamesdoesjavascript@gmail.com>
2025-09-03 13:48:37 -0500

    Added computer choice function

--- javascript.js
+++ javascript.js
@@ -11,3 +11,24 @@
 // Run switch statement to figure which option, if none
 // default to ask again.
 
+// Function to get computer choice, which relies on switch statement.
+function getComputerChoice()
+{
+    let computerChoice = "";
+    value =  Math.floor(Math.random() * 3)
+    switch(value)
+        {
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2:
+            computerChoice = "Cooler";
+            break;
+        }
+    return computerChoice;
+}
+
+
