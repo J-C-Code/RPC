6f2aa96be3e2d6626ed244a3a30ea09a91d6133e
James Collins <jamesdoesjavascript@gmail.com>
2025-09-03 11:09:21 -0500

    Added readme and psuedocode basics.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rock Paper Cooler
+
+- Paper beats Rock
+    - Paper loses to Cooler
+
+- Rock beats Cooler
+    - Rock loses to PAper
+
+- Cooler beats Paper
+    - Cooler loses to Rock
--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 // This will be a console javscript game.
+
+// We will need to get user input, then check if it equals
+// rock paper or cooler. 
+
+
+// Get User Input
+
+// Change it to all lowercase 
+
+// Run switch statement to figure which option, if none
+// default to ask again.
+
